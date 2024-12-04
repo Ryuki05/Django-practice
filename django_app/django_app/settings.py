7fb75d5a105afee3310d23e62c6c296dc7206c5c
Ryuki05 <higashiking05@gmail.com>
2024-12-04 14:00:07 +0900

    cr

--- django_app/django_app/settings.py
+++ django_app/django_app/settings.py
@@ -75,8 +75,15 @@ WSGI_APPLICATION = 'django_app.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'psysdb',
+        'USER': 'root',
+        'PASSWORD': 'Pa$$w0rd',
+            'HOST': 'localhost',
+            'PORT': '3306',
+        'OPTIONS': {
+            'init_command':"SET sql_mode='STRICT_TRANS_TABLES'",
+        }
     }
 }
 
